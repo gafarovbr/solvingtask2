f9985908e8f74caeaf02e4dbde837987e1117615
gafarovbr <bulat.gafarov95@mail.ru>
2023-08-30 23:37:53 +0300

    Solving problems from paragraph 1.3

---
+++ 1.3.1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    //std::cout << "C+";
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n  - is a better C\n  - supports data abstraction\n  - supports object-oriented programming\n  - supports generic programming.\n";
+}
---
+++ 1.3.2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int num1, num2;
+
+int main()
+{
+    std:: cin >> num1 >> num2;
+    std:: cout << num1 + num2;
+
+}
